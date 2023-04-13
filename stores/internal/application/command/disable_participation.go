d5a88a1e34ff10fa8be61417e18174adeb685a48
Samuel <samuel.mebrahtu@atos.net>
2023-04-13 11:30:20 +0100

    add: stores' disable_participation app command

---
+++ stores/internal/application/command/disable_participation.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"context"
+	"eda-in-go/stores/internal/domain"
+)
+
+type DisableParticipation struct {
+	ID string
+}
+
+type DisableParticipationHandler struct {
+	stores domain.StoreRepository
+}
+
+func NewDisableParticipationHandler(stores domain.StoreRepository) DisableParticipationHandler {
+	return DisableParticipationHandler{stores: stores}
+}
+
+func (h DisableParticipationHandler) DisableParticipation(ctx context.Context, cmd DisableParticipation) error {
+	store, err := h.stores.Find(ctx, cmd.ID)
+	if err != nil {
+		return err
+	}
+
+	err = store.DisableParticipation()
+	if err != nil {
+		return err
+	}
+
+	return h.stores.Update(ctx, store)
+}
